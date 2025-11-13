617183f615ae830d73081ad8d809877bb8349b4f
m1cva <114001753+m1cva@users.noreply.github.com>
2025-11-13 19:25:53 +0400

    Update base URL in vite.config.ts
    Set base URL for GitHub repository in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/derevya-project-main/', // 🔹 ОБЯЗАТЕЛЬНО: имя твоего репозитория на GitHub
+
   server: {
     host: "::",
     port: 8080,
